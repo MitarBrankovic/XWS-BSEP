b78bc500bd820f4691dec666893db7791f715d92
piwneuh <filip.pinjuh@gmail.com>
2022-05-22 16:53:39 +0200

    feat: frontend service

---
+++ Frontend/dislinkt/src/app/services/user.service.ts
@@ -0,0 +1,26 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Router } from "@angular/router";
+import { User } from "../model/user.model";
+
+@Injectable({
+    providedIn: 'root',
+  })
+  export class UserService {
+    private _url = 'http://localhost:8000';
+  
+    constructor(private http: HttpClient, private router: Router) {}
+
+    public login(user: User) {
+      return this.http.post(this._url + '/login', user);
+    }
+
+    public register(user: User) {
+    	return this.http.post(this._url + '/register', user);
+    }
+
+		public passwordlessLogin(username: string) {
+			return this.http.get(this._url + '/loginDemand');
+		}
+
+  }
