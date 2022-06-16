1bbe6b4bb9b636ebff846b0f2d88c7fed66d19ad
piwneuh <filip.pinjuh@gmail.com>
2022-06-16 23:45:26 +0200

    feat: sonar

--- Agent/src/main/java/com/example/agent/security/WebConfig.java
+++ Agent/src/main/java/com/example/agent/security/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
+                .allowedOrigins("http://localhost:4201")
                 .allowedMethods("GET", "PUT", "POST", "PATCH", "DELETE", "OPTIONS");
     }
 }
